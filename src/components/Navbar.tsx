f1dda54d65752596e9652c9e7bb4627cfc1cddd6
Cezar Luca <cezar.luca@hotmail.com>
2025-07-19 13:47:47 +0200

    Set up the layout for the Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,35 @@
+import { navLinks } from "@/app/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <nav>
+            <div>
+                <Link href="#home" className="flex items-center gap-2">
+                    <Image
+                        src="/images/logo.png"
+                        alt="Velvet Pour Logo"
+                        width={25}
+                        height={25}
+                    />
+                    <p>Velvet Pour</p>
+                </Link>
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <Link
+                                href={`#${link.id}`}
+                                className="hover:text-gray-300"
+                            >
+                                {link.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
