82e6fd5c5982fbe461872c0221bcfa0d22de9e57
Cezar Luca <cezar.luca@hotmail.com>
2025-07-19 21:41:27 +0200

    Set up Navbar animation with GSAP

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,31 @@
 import { navLinks } from "@/app/constants";
 import Image from "next/image";
 import Link from "next/link";
+import { useGSAP } from "@gsap/react";
 
 const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: "nav",
+                start: "bottom top",
+            },
+        });
+
+        navTween.fromTo(
+            "nav",
+            {
+                backgroundColor: "transparent",
+            },
+            {
+                backgroundColor: "#0000050",
+                backgroundFilter: "blur(10px)",
+                duration: 1,
+                ease: "power1.inOut",
+            }
+        );
+    });
+
     return (
         <nav>
             <div>
