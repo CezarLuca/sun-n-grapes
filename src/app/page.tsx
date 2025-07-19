4b7c6a82ecc1e7f2624bb62acc0ef3001006edc9
Cezar Luca <cezar.luca@hotmail.com>
2025-07-19 11:51:03 +0200

    Fixed style issues and added Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/Navbar";
 import LandingSection from "@/sections/LandingSection";
 
 export default function Home() {
     return (
         <main>
+            <Navbar />
             <LandingSection />
         </main>
     );
--- src/sections/LandingSection.tsx
+++ src/sections/LandingSection.tsx
@@ -5,9 +5,9 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const LandingSection = () => {
     return (
-        <div className="fex-center h-[100vh]">
+        <div className="flex-center h-[100vh]">
             <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
-            <></>
+            <div className="text-xl text-gray-50">Technical Check</div>
         </div>
     );
 };
