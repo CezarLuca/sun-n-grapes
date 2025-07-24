436e374353ec5394585a50a4f2d6f027fc435844
Cezar Luca <cezar.luca@hotmail.com>
2025-07-24 23:49:11 +0200

    Added the animation video to the HeroSection component

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -43,37 +43,42 @@ const HeroSection = () => {
     }, []);
 
     return (
-        <section id="hero" className="noisy">
-            <h1 className="title">Grapes n&apos; Sun</h1>
-            <img
-                src="/images/hero-left-leaf.png"
-                alt="left-leaf"
-                className="left-leaf"
-            />
-            <img
-                src="/images/hero-right-leaf.png"
-                alt="right-leaf"
-                className="right-leaf"
-            />
-            <div className="body">
-                <div className="content">
-                    <div className="space-y-5 hidden md:block">
-                        <p>Cool. Crisp. Classic.</p>
-                        <p className="subtitle">
-                            Sip the Spirit <br /> of Summer
-                        </p>
-                    </div>
-                    <div className="view-cocktails">
-                        <p className="subtitle">
-                            Every cocktail on our menu is a blend of premium
-                            ingredients, creative flair, and timeless recipes -
-                            designed to delight your senses.
-                        </p>
-                        <Link href={`#cocktails`}>View Cocktails</Link>
+        <>
+            <section id="hero" className="noisy">
+                <h1 className="title">Grapes n&apos; Sun</h1>
+                <img
+                    src="/images/hero-left-leaf.png"
+                    alt="left-leaf"
+                    className="left-leaf"
+                />
+                <img
+                    src="/images/hero-right-leaf.png"
+                    alt="right-leaf"
+                    className="right-leaf"
+                />
+                <div className="body">
+                    <div className="content">
+                        <div className="space-y-5 hidden md:block">
+                            <p>Cool. Crisp. Classic.</p>
+                            <p className="subtitle">
+                                Sip the Spirit <br /> of Summer
+                            </p>
+                        </div>
+                        <div className="view-cocktails">
+                            <p className="subtitle">
+                                Every cocktail on our menu is a blend of premium
+                                ingredients, creative flair, and timeless
+                                recipes - designed to delight your senses.
+                            </p>
+                            <Link href={`#cocktails`}>View Cocktails</Link>
+                        </div>
                     </div>
                 </div>
+            </section>
+            <div className="video absolute inset-0">
+                <video src="/videos/input.mp4" />
             </div>
-        </section>
+        </>
     );
 };
 
