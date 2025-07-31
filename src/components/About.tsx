046807dc5eedef790a0a45c520d220678e470d6a
Cezar Luca <cezar.luca@hotmail.com>
2025-07-31 23:53:04 +0200

    Added the descriptive text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,7 +11,21 @@ const About = () => {
                             from muddle to garnish
                         </h2>
                     </div>
-                    <div className="sub-content"></div>
+                    <div className="sub-content">
+                        <p>
+                            Every cocktail tells a story, and at our bar, we
+                            believe in crafting each one with care and
+                            precision. From the selection of the finest
+                            ingredients to the artful presentation, we ensure
+                            that every sip is a journey of flavor and
+                            experience. Join us to explore a world where
+                            mixology meets passion, and every drink is a
+                            masterpiece.
+                        </p>
+                        <div>
+                            <p className="md:text-3xl"></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
