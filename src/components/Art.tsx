28d408aa5c5f1480875ffde79b5e4488bdeeb21c
Cezar Luca <cezar.luca@hotmail.com>
2025-08-03 13:44:22 +0200

    Implement the Art Section

--- src/components/Art.tsx
+++ src/components/Art.tsx
@@ -1,7 +1,44 @@
-import { goodLists } from "@/app/constants";
+import { featureLists, goodLists } from "@/app/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 /* eslint-disable @next/next/no-img-element */
 const Art = () => {
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+
+    useGSAP(() => {
+        const start = isMobile ? "top 20%" : "top top";
+
+        const maskTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#art",
+                start: start,
+                end: "bottom center",
+                scrub: 1.5,
+                pin: true,
+            },
+        });
+
+        maskTimeline
+            .to(".will-fade", {
+                opacity: 1,
+                stagger: 0.2,
+                ease: "power1.inOut",
+            })
+            .to(".masked-img", {
+                scale: 1.3,
+                maskPosition: "center",
+                maskSize: "400%",
+                duration: 1,
+                ease: "power1.inOut",
+            })
+            .to("#masked-content", {
+                opacity: 1,
+                duration: 1,
+                ease: "power1.inOut",
+            });
+    });
     return (
         <div id="art">
             <div className="container mx-auto h-full pt-20">
@@ -21,6 +58,27 @@ const Art = () => {
                             alt="cocktail"
                             className="abs-center masked-img size-full object-contain"
                         />
+                    </div>
+                    <ul className="space-y-4 will-fade">
+                        {featureLists.map((feature, index) => (
+                            <li
+                                key={index}
+                                className="flex items-center justify-start gap-2"
+                            >
+                                <img src="/images/check.png" alt="check" />
+                                <p className="md:w-fit w-60">{feature}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="masked-container">
+                    <h2 className="will-fade">Sip-Worthy Perfection</h2>
+                    <div id="masked-content">
+                        <h3>Made with Craft, Poured with Passion</h3>
+                        <p>
+                            This isn&apos;t just a drink. It&apos;s a carefully
+                            crafted moment made just for you.
+                        </p>
                     </div>
                 </div>
             </div>
