db126fe3d9d26dba0df573965135fb9291e1e0db
Cezar Luca <cezar.luca@hotmail.com>
2025-08-08 17:28:27 +0200

    Completed the content of the Footer section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { openingHours, socials } from "@/app/constants";
+
 /* eslint-disable @next/next/no-img-element */
 const Contact = () => {
     return (
@@ -24,6 +26,30 @@ const Contact = () => {
                     <h3>Contact Us</h3>
                     <p>(123) 456-7890</p>
                     <p>hello@cocktail.com</p>
+                </div>
+                <div>
+                    <h3>Open Every Day</h3>
+                    {openingHours.map((time) => (
+                        <p key={time.day}>
+                            {time.day}: {time.time}
+                        </p>
+                    ))}
+                </div>
+                <div>
+                    <h3>Socials</h3>
+                    <div className="flex-center gap-5">
+                        {socials.map((social) => (
+                            <a
+                                key={social.name}
+                                href={social.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                aria-label={social.name}
+                            >
+                                <img src={social.icon} alt={`${social.name}`} />
+                            </a>
+                        ))}
+                    </div>
                 </div>
             </div>
         </footer>
